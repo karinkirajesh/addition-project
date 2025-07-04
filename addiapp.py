c1d2494ec566a2cc37b73929f43029360de7a298
karinkirajesh <karinkirajesh55@gmail.com>
2025-07-04 11:21:25 +0530

    addedd soem comments as first commit

--- addiapp.py
+++ addiapp.py
@@ -1,5 +1,7 @@
 #ask the first number
 num1=int(input("Enter the first number :"))
+#ask the second number
 num2=int(input("Enter the second number :"))
+#addition operation
 sum=num1+num2
 print(f"Addition of two number is {sum}")
